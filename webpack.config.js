a19fb41d08cac75340023323b1dbb532152d546e
Annie Huang <annie.hn@gmail.com>
2019-12-14 18:18:49 +1100

    Create css configuration using css-loader only for now

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,13 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['css-loader'],
+            },
+        ],
+    },
 };
