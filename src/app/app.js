58d390a07465ec476121fe99743d0e6ca8a291a3
Annie Huang <annie.hn@gmail.com>
2019-12-14 12:21:15 +1100

    Refactor app/app.js to only call AlertService.js and ComponentService.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,26 +1,25 @@
-const numberOneInput = document.getElementById("numberOne");
-const numberTwoInput = document.getElementById("numberTwo");
-const addValuesButton = document.getElementById("addValues");
-const resultDiv = document.getElementById("result");
+import {AlertService} from "./alert.service";
+import {ComponentService} from "./component.service";
+import {parseInputs} from "./utils/parse-inputs";
+import {inputsAreValid} from "./utils/inputs-are-valid";
 
+const alertService = new AlertService();
+const componentService = new ComponentService();
+const run = (alertService, componentService) => {
+    alertService.hideErrors();
+    componentService.onClick(() => {
+        alertService.hideErrors();
+        const inputs = componentService.getInputs();
+        const parsedInputs = parseInputs(...inputs);
+        if (inputsAreValid(...parsedInputs)) {
+            const [numA, numB] = parsedInputs;
+            componentService.setResult(numA + numB);
+        } else {
+            componentService.setResult("");
+            alertService.handleAdditionError(inputs, parsedInputs);
+        }
+    });
 
+};
+run(alertService, componentService);
 
-
-
-
-
-
-
-hideErrors();
-addValuesButton.addEventListener("click", () => {
-    hideErrors();
-    const inputs = [numberOneInput.value, numberTwoInput.value];
-    const parsedInputs = parseInputs(...inputs);
-    if (inputsAreValid(...parsedInputs)) {
-        const [numA, numB] = parsedInputs;
-        resultDiv.innerText = numA + numB;
-    } else {
-        resultDiv.innerText = "";
-        handleAdditionError(inputs, parsedInputs);
-    }
-});
