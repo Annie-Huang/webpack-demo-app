3b732e3ee195af28894941bad85c9d9a33cdc1a8
Annie Huang <annie.hn@gmail.com>
2019-12-14 18:43:05 +1100

    Create main.css and include it in index.js. And add it into index.js. Checked the main.js and confirm the css content is appended into the end.

--- dist/main.js
+++ dist/main.js
@@ -86,6 +86,93 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./node_modules/css-loader/dist/runtime/api.js":
+/*!*****************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/api.js ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
+*/
+// css base code, injected by the css-loader
+// eslint-disable-next-line func-names
+module.exports = function (useSourceMap) {
+  var list = []; // return the list of modules as css string
+
+  list.toString = function toString() {
+    return this.map(function (item) {
+      var content = cssWithMappingToString(item, useSourceMap);
+
+      if (item[2]) {
+        return "@media ".concat(item[2], " {").concat(content, "}");
+      }
+
+      return content;
+    }).join('');
+  }; // import a list of modules into the list
+  // eslint-disable-next-line func-names
+
+
+  list.i = function (modules, mediaQuery) {
+    if (typeof modules === 'string') {
+      // eslint-disable-next-line no-param-reassign
+      modules = [[null, modules, '']];
+    }
+
+    for (var i = 0; i < modules.length; i++) {
+      var item = [].concat(modules[i]);
+
+      if (mediaQuery) {
+        if (!item[2]) {
+          item[2] = mediaQuery;
+        } else {
+          item[2] = "".concat(mediaQuery, " and ").concat(item[2]);
+        }
+      }
+
+      list.push(item);
+    }
+  };
+
+  return list;
+};
+
+function cssWithMappingToString(item, useSourceMap) {
+  var content = item[1] || ''; // eslint-disable-next-line prefer-destructuring
+
+  var cssMapping = item[3];
+
+  if (!cssMapping) {
+    return content;
+  }
+
+  if (useSourceMap && typeof btoa === 'function') {
+    var sourceMapping = toComment(cssMapping);
+    var sourceURLs = cssMapping.sources.map(function (source) {
+      return "/*# sourceURL=".concat(cssMapping.sourceRoot).concat(source, " */");
+    });
+    return [content].concat(sourceURLs).concat([sourceMapping]).join('\n');
+  }
+
+  return [content].join('\n');
+} // Adapted from convert-source-map (MIT)
+
+
+function toComment(sourceMap) {
+  // eslint-disable-next-line no-undef
+  var base64 = btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap))));
+  var data = "sourceMappingURL=data:application/json;charset=utf-8;base64,".concat(base64);
+  return "/*# ".concat(data, " */");
+}
+
+/***/ }),
+
 /***/ "./src/app/alert.service.js":
 /*!**********************************!*\
   !*** ./src/app/alert.service.js ***!
@@ -238,17 +325,36 @@ const parseInputs = (...input) => {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _app_alert_service__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./app/alert.service */ "./src/app/alert.service.js");
-/* harmony import */ var _app_component_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./app/component.service */ "./src/app/component.service.js");
-/* harmony import */ var _app_app__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app */ "./src/app/app.js");
+/* harmony import */ var _app_app__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./app/app */ "./src/app/app.js");
+/* harmony import */ var _main_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./main.css */ "./src/main.css");
+/* harmony import */ var _main_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_main_css__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _app_alert_service__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/alert.service */ "./src/app/alert.service.js");
+/* harmony import */ var _app_component_service__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app/component.service */ "./src/app/component.service.js");
 
 
 
 
-const alertService = new _app_alert_service__WEBPACK_IMPORTED_MODULE_0__["AlertService"]();
-const componentService = new _app_component_service__WEBPACK_IMPORTED_MODULE_1__["ComponentService"]();
 
-Object(_app_app__WEBPACK_IMPORTED_MODULE_2__["run"])(alertService, componentService);
+const alertService = new _app_alert_service__WEBPACK_IMPORTED_MODULE_2__["AlertService"]();
+const componentService = new _app_component_service__WEBPACK_IMPORTED_MODULE_3__["ComponentService"]();
+
+Object(_app_app__WEBPACK_IMPORTED_MODULE_0__["run"])(alertService, componentService);
+
+
+/***/ }),
+
+/***/ "./src/main.css":
+/*!**********************!*\
+  !*** ./src/main.css ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+exports = module.exports = ___CSS_LOADER_API_IMPORT___(false);
+// Module
+exports.push([module.i, "body {\r\n    background: purple;\r\n}\r\n", ""]);
 
 
 /***/ })
