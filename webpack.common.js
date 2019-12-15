21ec3458b9fbf6d801f8dad3b5d2cca56d20e251
Annie Huang <annie.hn@gmail.com>
2019-12-15 21:28:08 +1100

    Add file-loader into webpack.common.js. run webpack and img tag with hash (<img src="imgs/webpack.5a2bd87008f02c88ba55c698d5b412b2.svg"/>) got injected into  the dist/index.html

--- webpack.common.js
+++ webpack.common.js
@@ -19,6 +19,8 @@ module.exports = {
                     'sass-loader'   // 1. Turns sass into css
                 ]
             },
+
+
             // {
             //     test: /\.(html)$/,
             //     use: {
@@ -29,7 +31,41 @@ module.exports = {
             {
                 test: /\.(html)$/,
                 use: ['html-loader']
+            },
+
+
+            // {
+            //     test: /\.(svg|png|jpe?g|gif)$/i,
+            //     use: [
+            //         {
+            //             loader: 'file-loader',
+            //         },
+            //     ],
+            // },
+            // {
+            //     test: /\.(svg|png|jpe?g|gif)$/i,
+            //     loader: 'file-loader',
+            //     options: {
+            //         outputPath: 'images',
+            //     },
+            // },
+            // This is the same as above.
+            // I think for webpack, [] and {} under use is interchangeable . very strange.
+            // Have to add esModule: false because since file-loader ^5.0.2,
+            // By default, file-loader generates JS modules that use the ES modules syntax.
+            // There are some cases in which using ES modules is beneficial, like in the case of module concatenation and tree shaking.
+            {
+                test: /\.(svg|png|jpe?g|gif)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[hash].[ext]',
+                        outputPath: 'imgs',
+                        esModule: false
+                    },
+                },
             }
+
         ],
     },
 };
