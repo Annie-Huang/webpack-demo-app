ffe90534e512740b77c0a37b8cac0f8d2de01ecc
Annie Huang <annie.hn@gmail.com>
2019-12-15 11:25:15 +1100

    Add     plugins: [new HtmlWebpackPlugin()], into webpack.config.js and you can see a default dist/index.html is generated

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -9,6 +10,7 @@ module.exports = {
         filename: "main.[contentHash].js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [new HtmlWebpackPlugin()],
     module: {
         rules: [
             // {
