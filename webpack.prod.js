e1dccff319ce79756b9f8c5f7665f13cd1f36b71
Annie Huang <annie.hn@gmail.com>
2019-12-16 16:54:52 +1100

    Configure mini-css-extract-plugin in webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,18 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
     mode: "production",
-    plugins: [new CleanWebpackPlugin()],
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].[contentHash].css'
+        }),
+        new CleanWebpackPlugin(),
+    ],
     output: {
         // filename: "main.js",
         // filename: "main.[contentHash].js",
